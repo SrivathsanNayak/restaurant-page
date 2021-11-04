12fee4945a002c1dfebf1e6a17b8a4612c3396c9
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-04 17:41:04 +0530

    Added draft content to index.js to check webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-console.log("If you can see this, the configuration works.");
-console.log("What about this one?");
+const content = document.getElementById("content");
+const heading = document.createElement("div");
+const restaurantName = document.createElement("h1");
+const restaurantTagline = document.createElement("h3");
+const about = document.createElement("div");
+const restaurantDescription = document.createElement("p");
+
+heading.append(restaurantName, restaurantTagline);
+about.append(restaurantDescription);
+content.append(heading, about);
+
+restaurantName.textContent = "Just a Restaurant";
+restaurantTagline.textContent = "An ordinary restaurant with an ordinary website!";
+restaurantDescription.textContent = "This is an ordinary restaurant. You can get some ordinary food here."
