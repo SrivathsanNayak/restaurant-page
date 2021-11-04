281c0757466534b1bbd30ed08239bc94a7305944
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-04 06:56:52 +0530

    First commit

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+console.log("If you can see this, the configuration works."),console.log("What about this one?");
---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log("If you can see this, the configuration works.");
+console.log("What about this one?");
