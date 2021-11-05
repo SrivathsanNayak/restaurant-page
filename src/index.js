6fbc6ae02dd6ebec28d1f1ca1ce85c635954f725
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-05 18:19:36 +0530

    Shifted code from index.js to pageLoad.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
-import printTest from './pageLoad.js';
-import image1 from './assets/onam-sadya.jpeg';
+import loadingContent from './pageLoad.js';
 
-const content = document.getElementById("content");
-const heading = document.createElement("div");
-const restaurantName = document.createElement("h1");
-const restaurantTagline = document.createElement("h3");
-const about = document.createElement("div");
-const restaurantDescription = document.createElement("p");
-const homePageImage = document.createElement("img");
-
-homePageImage.src = image1;
-homePageImage.onclick = printTest;
-
-heading.append(restaurantName, restaurantTagline);
-about.append(restaurantDescription);
-content.append(heading, about);
-content.append(homePageImage);
-
-restaurantName.textContent = "Just a Restaurant";
-restaurantTagline.textContent = "An ordinary restaurant with an ordinary website!";
-restaurantDescription.textContent = "This is an ordinary restaurant. You can get some ordinary food here."
+loadingContent();
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,3 +1,27 @@
-export default function printTest() {
-    console.log("If you're able to see this, you have successfully imported this.");
+import image1 from './assets/onam-sadya.jpeg';
+
+export default function loadingContent() {
+    const content = document.getElementById("content");
+    const heading = document.createElement("div");
+    const restaurantName = document.createElement("h1");
+    const restaurantTagline = document.createElement("h3");
+    const about = document.createElement("div");
+    const restaurantDescription = document.createElement("p");
+    const homePageImage = document.createElement("img");
+    homePageImage.src = image1;
+
+    heading.append(restaurantName, restaurantTagline);
+    about.append(restaurantDescription);
+    content.append(heading, about);
+    content.append(homePageImage);
+
+    restaurantName.textContent = "Just a Restaurant";
+    restaurantTagline.textContent = "An ordinary restaurant with an ordinary website!";
+    restaurantDescription.textContent = "This is an ordinary restaurant. You can get some ordinary food here."
 }
+
+
+
+
+
+
