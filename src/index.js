455a51a5a0f2de1808c8eccaa026020bfb11c668
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-05 15:12:10 +0530

    Added image, experimenting with modules

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+import printTest from './pageLoad.js';
+import image1 from './assets/onam-sadya.jpeg';
+
 const content = document.getElementById("content");
 const heading = document.createElement("div");
 const restaurantName = document.createElement("h1");
 const restaurantTagline = document.createElement("h3");
 const about = document.createElement("div");
 const restaurantDescription = document.createElement("p");
+const homePageImage = document.createElement("img");
+
+homePageImage.src = image1;
+homePageImage.onclick = printTest;
 
 heading.append(restaurantName, restaurantTagline);
 about.append(restaurantDescription);
 content.append(heading, about);
+content.append(homePageImage);
 
 restaurantName.textContent = "Just a Restaurant";
 restaurantTagline.textContent = "An ordinary restaurant with an ordinary website!";
---
+++ src/pageLoad.js
@@ -0,0 +1,3 @@
+export default function printTest() {
+    console.log("If you're able to see this, you have successfully imported this.");
+}
