2a847c2444068179da0d98ffcfc7f29502cc7996
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-08 12:27:59 +0530

    Changed image for cover page

--- src/homePage.js
+++ src/homePage.js
@@ -1,4 +1,4 @@
-import image1 from './assets/onam-sadya.jpeg';
+import image1 from './assets/homemadefood.jpg';
 
 function headingPart() {
     const heading = document.createElement("div");
@@ -7,11 +7,14 @@ function headingPart() {
     const about = document.createElement("p");
     const coverImage = document.createElement("img");
     coverImage.src = image1;
+    coverImage.alt = "Homemade food";
+
+    coverImage.classList.add('cover-image');
 
     heading.append(name, tagline, about, coverImage);
 
-    name.textContent = "Just a Restaurant";
-    tagline.textContent = "An ordinary restaurant with an ordinary website!";
+    name.textContent = "Mother's Kitchen";
+    tagline.textContent = "Food that makes you feel at home.";
     about.textContent = "This is an ordinary restaurant. You can get some ordinary food here.";
 
     return heading;
