c94a5b55e9cdb645a6ebad32ab6531bd24279171
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-11 20:54:20 +0530

    Added menu skeleton

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,8 +1,26 @@
 function menu() {
-    const menuDiv = document.createElement("div");
-    menuDiv.textContent = "Menu content";
+    const menuPart = document.createElement("div");
+    const breakfastDiv = document.createElement("div");
+    const lunchDiv = document.createElement("div");
+    const dinnerDiv = document.createElement("div");
+    const breakfastTitle = document.createElement("h3");
+    const lunchTitle = document.createElement("h3");
+    const dinnerTitle = document.createElement("h3");
 
-    return menuDiv;
+    breakfastTitle.classList.add('menu-titles');
+    lunchTitle.classList.add('menu-titles');
+    dinnerTitle.classList.add('menu-titles');
+
+    breakfastDiv.append(breakfastTitle);
+    lunchDiv.append(lunchTitle);
+    dinnerDiv.append(dinnerTitle);
+    menuPart.append(breakfastDiv, lunchDiv, dinnerDiv);
+
+    breakfastTitle.textContent = "Breakfast";
+    lunchTitle.textContent = "Lunch";
+    dinnerTitle.textContent = "Dinner";
+
+    return menuPart;
 }
 
 export {menu};
