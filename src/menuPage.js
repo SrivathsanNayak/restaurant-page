a7b118c5168ceef4cccb677ee2ca4001d099cde0
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-12 12:18:52 +0530

    Added functions replacing old code

--- src/menuPage.js
+++ src/menuPage.js
@@ -10,47 +10,39 @@ function menu() {
     const breakfastDiv = document.createElement("div");
     const lunchDiv = document.createElement("div");
     const dinnerDiv = document.createElement("div");
-    const breakfastTitle = document.createElement("h3");
-    const lunchTitle = document.createElement("h3");
-    const dinnerTitle = document.createElement("h3");
+
+    breakfastDiv.append(createMenuTitle("Breakfast"),
+    createMenuItems("IDLI-VADA: ₹100", image1, "Idli and Vada"),
+    createMenuItems("MASALA DOSA: ₹120", image2, "Dosa filled with Masala"));
+
     
-    const breakfast1 = document.createElement("div");
-    const imageBreakfast1 = document.createElement("img");
-    const titleBreakfast1 = document.createElement("h4");
-    imageBreakfast1.src = image1;
-    imageBreakfast1.alt = "Idli and Vada"; 
 
-    const breakfast2 = document.createElement("div");
-    const imageBreakfast2 = document.createElement("img");
-    const titleBreakfast2 = document.createElement("h4");
-    imageBreakfast2.src = image2;
-    imageBreakfast2.alt = "Dosa filled with masala";
-
-    breakfastTitle.classList.add('menu-titles');
-    lunchTitle.classList.add('menu-titles');
-    dinnerTitle.classList.add('menu-titles');
-    breakfast1.classList.add('menu-divs');
-    breakfast2.classList.add('menu-divs');
-    imageBreakfast1.classList.add('menu-images');
-    imageBreakfast2.classList.add('menu-images');
-    titleBreakfast1.classList.add('menu-foods');
-    titleBreakfast2.classList.add('menu-foods');
-
-    breakfast1.append(imageBreakfast1, titleBreakfast1);
-    breakfast2.append(imageBreakfast2, titleBreakfast2);
-    breakfastDiv.append(breakfastTitle, breakfast1, breakfast2);
-    lunchDiv.append(lunchTitle);
-    dinnerDiv.append(dinnerTitle);
     menuPart.append(breakfastDiv, lunchDiv, dinnerDiv);
 
-    breakfastTitle.textContent = "Breakfast";
-    lunchTitle.textContent = "Lunch";
-    dinnerTitle.textContent = "Dinner";
-
-    titleBreakfast1.textContent = "IDLI-VADA: ₹100";
-    titleBreakfast2.textContent = "MASALA DOSA: ₹120";
-
     return menuPart;
+}
+
+function createMenuTitle(name) {
+    const title = document.createElement("h3");
+    title.classList.add('menu-titles');
+    title.textContent = name;
+    return title;
+}
+
+function createMenuItems(itemName, itemImage, itemImageAlt) {
+    const itemDiv = document.createElement("div");
+    const name = document.createElement("h4");
+    const image = document.createElement("img");
+    name.textContent = itemName;
+    image.src = itemImage;
+    image.alt = itemImageAlt;
+
+    itemDiv.classList.add('menu-divs');
+    name.classList.add('menu-foods');
+    image.classList.add('menu-images');
+
+    itemDiv.append(image, name);
+    return itemDiv;
 }
 
 export {menu};
