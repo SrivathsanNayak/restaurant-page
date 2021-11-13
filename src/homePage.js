b73bce123b1079c8c04301d84bdc4b7211c6d893
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-11-14 01:26:53 +0530

    Display-related changes in homepage

--- src/homePage.js
+++ src/homePage.js
@@ -5,8 +5,7 @@ function home() {
     const name = document.createElement("h1");
     const tagline = document.createElement("h3");
     const paraDiv = document.createElement("div");
-    const aboutPara1 = document.createElement("p");
-    const aboutPara2 = document.createElement("p");
+
     const coverImage = document.createElement("img");
     coverImage.src = image1;
     coverImage.alt = "Homemade food";
@@ -15,18 +14,23 @@ function home() {
     name.classList.add('title');
     tagline.classList.add('tagline');
     paraDiv.classList.add('para-div');
-    aboutPara1.classList.add('para');
-    aboutPara2.classList.add('para');
 
-    paraDiv.append(aboutPara1, aboutPara2, coverImage);
-    heading.append(name, tagline, paraDiv);
+    paraDiv.append(createPara("We specialize in delicious, homemade food that you can eat to your heart's content."),
+    createPara("Craving some good ol' homely meals? Check out our Menu section!"));
+    heading.append(name, tagline, paraDiv, coverImage);
 
     name.textContent = "Mother's Kitchen";
     tagline.textContent = "Food that makes you feel at home.";
-    aboutPara1.textContent = "We specialize in delicious, homemade food that you can eat to your heart's content.";
-    aboutPara2.textContent = "Craving some good ol' homely meals? Check out our Menu section!";
 
     return heading;
+}
+
+function createPara(text) {
+    const para = document.createElement("p");
+    para.classList.add('para');
+    
+    para.textContent = text;
+    return para;
 }
 
 export {home};
